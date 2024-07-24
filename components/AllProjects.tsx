eb4f1e1bb7b3ed63b394e7d1dbc48dae3d744dd6
asutosh-147 <behuriaasutosh7@gmail.com>
2024-07-24 15:27:02 +0530

    added project links

--- components/AllProjects.tsx
+++ components/AllProjects.tsx
@@ -4,8 +4,9 @@ import { useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
 import React, { useRef } from "react";
 import { MotionDiv } from "./MotionDiv";
+import Link from "next/link";
 type projectData = (typeof projectsData)[number];
-const Project = ({ description, imageUrl, tags, title }: projectData) => {
+const Project = ({ description, imageUrl, tags, title, link }: projectData) => {
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: ref,
@@ -30,25 +31,27 @@ const Project = ({ description, imageUrl, tags, title }: projectData) => {
       ref={ref}
       className="group relative h-72 w-full overflow-hidden rounded-lg bg-gray-100 p-5 leading-relaxed shadow-xl transition-all duration-300 odd:self-start even:self-end hover:bg-gray-300 lg:w-3/5 dark:bg-white/30 dark:text-gray-100 dark:hover:bg-white/40"
     >
-      <div className="flex h-full w-[70%] sm:w-[45%] flex-col">
-        <div className="text-xl font-semibold">{title}</div>
-        <div className="font-normal">{description}</div>
-        <ul className="mt-auto flex flex-wrap gap-2 text-sm">
-          {tags.map((tag, index) => (
-            <li
-              key={index}
-              className="flex cursor-default rounded-lg bg-gray-500 p-1 font-semibold text-gray-200 transition-all duration-300 hover:bg-gray-800 dark:bg-gray-800 dark:hover:bg-gray-700"
-            >
-              {tag}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <Image
-        src={imageUrl}
-        alt={title}
-        className="absolute -right-44 bottom-10 sm:-right-40 sm:top-9 w-[18rem] sm:w-[28rem] rotate-3 rounded-lg shadow-2xl transition-all duration-300 group-hover:-translate-x-5 group-hover:translate-y-1 group-hover:rotate-0"
-      />
+      <Link href={link} target="_blank">
+        <div className="flex h-full w-[70%] flex-col sm:w-[45%]">
+          <div className="text-xl font-semibold">{title}</div>
+          <div className="font-normal">{description}</div>
+          <ul className="mt-auto flex flex-wrap gap-2 text-sm">
+            {tags.map((tag, index) => (
+              <li
+                key={index}
+                className="flex cursor-default rounded-lg bg-gray-500 p-1 font-semibold text-gray-200 transition-all duration-300 hover:bg-gray-800 dark:bg-gray-800 dark:hover:bg-gray-700"
+              >
+                {tag}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <Image
+          src={imageUrl}
+          alt={title}
+          className="absolute -right-44 bottom-10 w-[18rem] rotate-3 rounded-lg shadow-2xl transition-all duration-300 group-hover:-translate-x-5 group-hover:translate-y-1 group-hover:rotate-0 sm:-right-40 sm:top-9 sm:w-[28rem]"
+        />
+      </Link>
     </MotionDiv>
   );
 };
